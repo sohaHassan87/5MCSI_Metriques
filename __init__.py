a6a29bf762e3d0c223cdce2bdf305d40f100a5ee
sohaHassan87 <122526619+sohaHassan87@users.noreply.github.com>
2024-06-07 12:06:19 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -28,12 +28,10 @@ def meteo():
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 
+@app.route("/commits/")
+def commits():
+    return render_template("commits.html")
 
-@app.route('/commits/')
-def extract_minutes(date_string):
-        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
-        minutes = date_object.minute
-        return jsonify({'minutes': minutes})
 
 
 @app.route("/rapport/")
